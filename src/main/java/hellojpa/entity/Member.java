885b56c602989ce36f5630d74aca76c3c3dbbd61
hyunmin0317 <choihm9903@naver.com>
2024-02-24 14:19:22 +0900

    feat: JPA save 메서드 작성

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -2,12 +2,14 @@ package hellojpa.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
     @Id
     private Long id;
---
+++ src/main/java/hellojpa/repository/JpaRepository02.java
@@ -0,0 +1,28 @@
+package hellojpa.repository;
+
+import hellojpa.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class JpaRepository02 {
+
+    public static void save(Member member) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}

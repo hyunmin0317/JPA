5bdd3442a9a9333d6937f092f6888a663f5fbaf1
hyunmin0317 <choihm9903@naver.com>
2024-02-24 15:40:55 +0900

    feat: 준영속 상태로 만드는 방법 (#3)

--- src/main/java/hellojpa/JpaMain03.java
+++ src/main/java/hellojpa/JpaMain03.java
@@ -128,4 +128,28 @@ public class JpaMain03 {
         }
         emf.close();
     }
+
+    // 준영속 상태로 만드는 방법
+    public static void test6() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            em.detach(member);  // 특정 엔티티만 준영속 상태로 전환
+            em.clear();         // 영속성 컨텍스트를 완전히 초기화
+            em.close();         // 영속성 컨텍스트를 종료
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
 }

e3b96575addf04d8d39528f049a95ced37c1828c
hyunmin0317 <choihm9903@naver.com>
2024-02-24 14:25:05 +0900

    test: JPA findOne 메서드 테스트

--- src/main/java/hellojpa/JpaMain02.java
+++ src/main/java/hellojpa/JpaMain02.java
@@ -7,6 +7,7 @@ public class JpaMain02 {
 
     public static void main(String[] args) {
         saveTest();
+        findOneTest();
     }
 
     public static void saveTest() {
@@ -15,5 +16,11 @@ public class JpaMain02 {
                 .name("HelloA")
                 .build();
         JpaRepository02.save(member);
+    }
+
+    public static void findOneTest() {
+        Member findMember = JpaRepository02.findOne(1L);
+        System.out.println("findMember.id = " + findMember.getId());
+        System.out.println("findMember.name = " + findMember.getName());
     }
 }

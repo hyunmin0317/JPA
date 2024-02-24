27067db132a54dece495dc4cb0cf6e25ccc28ee4
hyunmin0317 <choihm9903@naver.com>
2024-02-24 16:21:03 +0900

    feat: User 엔티티 작성 (#4)

---
+++ src/main/java/hellojpa/entity/User.java
@@ -0,0 +1,35 @@
+package hellojpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name", unique = true, length = 10)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+}

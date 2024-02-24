41db43ceb2122d1b7a749c0d4ca142d2855bea82
hyunmin0317 <choihm9903@naver.com>
2024-02-24 16:13:25 +0900

    feat: 엔티티 매핑 (#4)

--- src/main/java/hellojpa/entity/Member2.java
+++ src/main/java/hellojpa/entity/Member2.java
@@ -3,17 +3,22 @@ package hellojpa.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Member {
+@Table(name = "MBR")
+public class Member2 {
 
     @Id
     private Long id;
 
+    @Column(unique = true, length = 10)
     private String name;
 
     public void setName(String name) {

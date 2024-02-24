44bafe37d94f20f7d1cda66e9da085e8f8e8aad5
hyunmin0317 <choihm9903@naver.com>
2024-02-24 15:03:36 +0900

    feat: 영속성 컨텍스트 개념 (#3)

---
+++ src/main/java/hellojpa/JpaMain03.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import hellojpa.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain03 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            //비영속
+            Member member = Member.builder()
+                    .id(101L)
+                    .name("HelloJPA")
+                    .build();
+
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            //영속성 컨텍스트에서 조회 (entityManager - 1차 캐시)
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}

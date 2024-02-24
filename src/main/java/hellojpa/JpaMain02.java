bd214bd48a9e31b4e2d4d24197de4f42905fc5eb
hyunmin0317 <choihm9903@naver.com>
2024-02-24 14:40:52 +0900

    test: JPA findAll 메서드 테스트

--- src/main/java/hellojpa/JpaMain02.java
+++ src/main/java/hellojpa/JpaMain02.java
@@ -3,12 +3,15 @@ package hellojpa;
 import hellojpa.entity.Member;
 import hellojpa.repository.JpaRepository02;
 
+import java.util.List;
+
 public class JpaMain02 {
 
     public static void main(String[] args) {
         saveTest();
         findOneTest();
         updateTest();
+        findAllTest();
     }
 
     public static void saveTest() {
@@ -23,6 +26,11 @@ public class JpaMain02 {
         Member member = JpaRepository02.findOne(1L);
         System.out.println("findMember.id = " + member.getId());
         System.out.println("findMember.name = " + member.getName());
+    }
+
+    public static void findAllTest() {
+        List<Member> result = JpaRepository02.findAll();
+        result.forEach(m -> System.out.println(m.getName()));
     }
 
     public static void updateTest() {

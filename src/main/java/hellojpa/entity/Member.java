2e198698a66234f63d17691ca3ccd127d5537e8a
hyunmin0317 <choihm9903@naver.com>
2024-02-24 16:10:01 +0900

    feat: 엔티티와 매핑할 테이블 지정 (#4)

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -1,16 +1,21 @@
 package hellojpa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "MBR")
 public class Member {
+
     @Id
     private Long id;
+
     private String name;
 
     public void setName(String name) {

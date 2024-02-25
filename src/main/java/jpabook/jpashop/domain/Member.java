1a948c015f170dc3c6858e02e06de6b60af785df
hyunmin0317 <choihm9903@naver.com>
2024-02-25 10:39:04 +0900

    feat: 실전 예제 1 - 엔티티 생성 (#4)

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+}

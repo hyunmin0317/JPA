d02b5bebf7f7f1c859a8130ef53e06b87166b62d
hyunmin0317 <choihm9903@naver.com>
2024-02-26 22:42:33 +0900

    feat: MappedSuperclass 매핑 정보 상속 (#7)

--- src/main/java/hellojpa/JpaMain07.java
+++ src/main/java/hellojpa/JpaMain07.java
@@ -1,15 +1,19 @@
 package hellojpa;
 
+import hellojpa.entity.Member3;
 import hellojpa.entity.Movie;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.time.LocalDateTime;
+
 public class JpaMain07 {
 
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     public static void test1() {
@@ -32,6 +36,28 @@ public class JpaMain07 {
             Movie findMovie = em.find(Movie.class, movie.getId());
             System.out.println("findMovie = " + findMovie);
 
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void test2() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member3 member = Member3.builder()
+                    .name("user1")
+                    .build();
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+
+            em.persist(member);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();

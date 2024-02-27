ba2f944622fee89960a85804668937f953066b52
hyunmin0317 <choihm9903@naver.com>
2024-02-27 19:41:57 +0900

    feat: 상속관계 매핑 설정 (#7)

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -11,6 +12,7 @@ public class JpaMain {
 
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     public static void test1() {
@@ -21,6 +23,25 @@ public class JpaMain {
         try {
             Order order = new Order();
             order.addOrderItem(new OrderItem());
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void test2() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();

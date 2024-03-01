642f63b0cce3ed56a634c0dd7e71ff72aca303ff
hyunmin0317 <choihm9903@naver.com>
2024-03-01 15:40:19 +0900

    feat: 페치 조인 기본 실습 (#11)

--- src/main/java/jpql/JpaMain11.java
+++ src/main/java/jpql/JpaMain11.java
@@ -13,7 +13,8 @@ import java.util.List;
 public class JpaMain11 {
 
     public static void main(String[] args) {
-        test1();
+//        test1();
+        test2();
     }
 
     public static void test1() {
@@ -53,6 +54,80 @@ public class JpaMain11 {
             String query4 = "select m.username from JPQL_TEAM t join t.members m";
             List<String> result4 = em.createQuery(query4, String.class).getResultList();
             result4.forEach(System.out::println);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void test2() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+            String query1 = "select m from JPQL_MEMBER m";
+            List<Member> result1 = em.createQuery(query1, Member.class).getResultList();
+            result1.forEach(m -> {
+                // 지연 로딩
+                System.out.println("member = " + member3.getUsername() + ", " + member3.getTeam().getName());
+                // 회원1, 팀A (SQL)
+                // 회원2, 팀A (1차 캐시)
+                // 회원3, 팀B (SQL)
+                // 회원 100명 -> N + 1
+            });
+
+            String query2 = "select m from JPQL_MEMBER m join fetch m.team";
+            List<Member> result2 = em.createQuery(query2, Member.class).getResultList();
+            result2.forEach(m -> {
+                // 즉시 로딩 (fetch)
+                System.out.println("member = " + member3.getUsername() + ", " + member3.getTeam().getName());
+            });
+
+            String query3 = "select t from JPQL_TEAM t join fetch t.members";
+            List<Team> result3 = em.createQuery(query3, Team.class).getResultList();
+            result3.forEach(team -> {
+                System.out.println("team = " + team.getName() + " | memebers = " + team.getMembers().size());
+                team.getMembers().forEach(member -> System.out.println("-> member = " + member));
+            });
+
+            String query4 = "select distinct t from JPQL_TEAM t join fetch t.members";
+            List<Team> result4 = em.createQuery(query4, Team.class).getResultList();
+            result4.forEach(team -> {
+                System.out.println("team = " + team.getName() + " | memebers = " + team.getMembers().size());
+                team.getMembers().forEach(member -> System.out.println("-> member = " + member));
+            });
 
             tx.commit();
         } catch (Exception e) {
